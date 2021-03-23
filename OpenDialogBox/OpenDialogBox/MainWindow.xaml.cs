df1e4b39a7523537c8d4d404222b77b98cdfd5f0
RON DOLGOY <dolgoyron@gmail.com>
2021-03-23 23:39:06 +0200

    Implemented OPEN CSV PATH using OpenDialogBox waiting for view model to complete 1st user case.

---
+++ OpenDialogBox/OpenDialogBox/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Microsoft.Win32; // FileDialog 
+
+namespace OpenDialogBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // private ViewModel vm;
+        public MainWindow()
+        {
+            InitializeComponent();
+            // vm = new ViewModel();
+        }
+        /*
+         * This method opens the path to the CSV file using a dialog box
+         */
+        private void OpenFileDialog_Click(object sender, RoutedEventArgs e)
+        {   
+            // Creating File Dialog object to interact with the files on the system
+            OpenFileDialog fDia = new OpenFileDialog();
+            fDia.Multiselect = false;
+            // Filtering for the relevent extenstions 
+            fDia.Filter = "CSV Files|*.csv| Excel Files|*.xlsx";
+ 
+            Nullable<bool> fDiaOK = fDia.ShowDialog();
+            if (fDiaOK == true) // File Dialog opened safely
+            {
+                FilePathBox.Text = fDia.FileNames[0] + ";";
+                // Turn upload button visible once a path was created.
+                UploadFileBox.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // vm.sendCSVPath(FilePathBox.Text);
+            Console.WriteLine("Done");
+        }
+    }
+}
