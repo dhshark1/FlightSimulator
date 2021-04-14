b92bd6c6e1fb43b924d9d97fe13714890588c558
Tom Magdaci <71394850+TomMagdaci@users.noreply.github.com>
2021-04-14 20:16:41 +0300

    Update ViewModel.cs

--- WpfApp1/WpfApp1/ViewModel.cs
+++ WpfApp1/WpfApp1/ViewModel.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel;
-using LiveCharts;//chart
-using LiveCharts.Defaults;
-using LiveCharts.Wpf;
 using OxyPlot;
 using OxyPlot.Series;
 namespace WpfApp1
